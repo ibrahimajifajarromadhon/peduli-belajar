3c60e6c7b618668237a09fea2f8119fc2b0b9ef6
MeyTeplon <ASUS@LAPTOP-VTDSD35M>
2023-11-26 12:00:32 +0700

    done design register page

--- src/layout/Authenticaton.jsx
+++ src/layout/Authenticaton.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
-import AuthImage from '../components/AuthImage'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import AuthImage from "../components/AuthImage";
+import { Outlet } from "react-router-dom";
 
 function Authenticaton() {
   return (
-    <div>
-        <AuthImage />
+    <div className="d-flex vh-100">
+      <div
+        className="contain d-flex justify-content-center align-items-center"
+        style={{ width: "100%" }}
+      >
         <Outlet />
-      
+      </div>
+      <div
+        className="image d-lg-flex d-none justify-content-center align-items-center"
+        style={{ width: "50%", backgroundColor: `var(--primary-purple)` }}
+      >
+        <div className="d-flex justify-content-center w-100">
+          <AuthImage />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Authenticaton
+export default Authenticaton;
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -3,11 +3,85 @@ import { Link } from "react-router-dom";
 
 function Register() {
   return (
-    <div>
-      <p>form register</p>
-      <Link to={"/login"} className="btn btn-success">
-        back to login
-      </Link>
+    <div className="register w-50 p-3 d-flex flex-column justify-content-center">
+      <h4 style={{ color: `var(--primary-purple)`, textAlign: "center" }}>Daftar</h4>
+      <div className="mb-3">
+        <label htmlFor="formGroupExampleInput" className="form-label">
+          Nama
+        </label>
+        <input
+          type="text"
+          className="form-control rounded-4"
+          id="formGroupExampleInput"
+          placeholder="Jhon Doe"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="formGroupExampleInput2" className="form-label">
+          Email
+        </label>
+        <input
+          type="email"
+          className="form-control rounded-4"
+          id="formGroupExampleInput2"
+          placeholder="jhonDoe@gmail.com"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="formGroupExampleInput3" className="form-label">
+          Nomor Telepon
+        </label>
+        <input
+          type="tel"
+          className="form-control rounded-4"
+          id="formGroupExampleInput3"
+          placeholder="+62 8...."
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="formGroupExampleInput4" className="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          className="form-control rounded-4"
+          id="formGroupExampleInput4"
+          placeholder="........"
+        />
+      </div>
+      <div className="mb-3">
+        <Link
+          to={"/login"}
+          className="btn rounded-4 text-light"
+          style={{ backgroundColor: `var(--primary-purple)`, width: "100%" }}
+        >
+          Daftar
+        </Link>
+      </div>
+      <hr />
+      <div className="text-center">
+        <p>
+          Sudah punya akun?{" "}
+          <span>
+            <Link to={"/login"} style={{ textDecoration: "none", color: `var(--primary-purple)`, fontWeight: "bold" }}>
+              Masuk di sini
+            </Link>
+          </span>
+        </p>
+      </div>
+
+      {/* Media Query for Large Screens */}
+      <style jsx>
+        {`
+          @media (max-width: 628px) {
+            .register {
+              width: 100% !important;
+              height: 100% !important;
+              background-color: var(--primary-young-blue) !important;
+            }
+          }
+        `}
+      </style>
     </div>
   );
 }
