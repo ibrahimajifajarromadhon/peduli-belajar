ba57a81ab7edf32c507a331f730b958c194cb314
Ayuni Tiyasari <ayutiyassari@gmail.com>
2023-12-05 20:23:48 +0700

    update validasi login

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,11 +1,76 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  FaCheckCircle,
+  FaTimesCircle,
+  FaEye,
+  FaEyeSlash,
+} from "react-icons/fa";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordValid, setPasswordValid] = useState(false);
+  const [validEmail, setValidEmail] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [isRegisteredEmail, setIsRegisteredEmail] = useState(true);
+  const [incorrectPassword, setIncorrectPassword] = useState(false);
+  const navigate = useNavigate();
+
+  const handleEmailChange = (event) => {
+    const newEmail = event.target.value;
+    setEmail(newEmail);
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (emailRegex.test(newEmail)) {
+      setValidEmail(true);
+      setIsRegisteredEmail(checkIfEmailIsRegistered(newEmail));
+    } else {
+      setValidEmail(false);
+    }
+  };
+
+  const handlePasswordChange = (event) => {
+    const newPassword = event.target.value;
+    setPassword(newPassword);
+
+    if (newPassword.length >= 8) {
+      setPasswordValid(true);
+    } else {
+      setPasswordValid(false);
+    }
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const checkIfEmailIsRegistered = (checkEmail) => {
+    const registeredEmails = ["test@user.com"];
+    return registeredEmails.includes(checkEmail);
+  };
+
+  const handleLogin = () => {
+    const isAuthenticated = authenticateUser(email, password);
+
+    if (!isAuthenticated) {
+      setIncorrectPassword(true);
+    } else {
+      setIncorrectPassword(false);
+      navigate("/myClass");
+    }
+  };
+
+  const authenticateUser = (enteredEmail, enteredPassword) => {
+    return enteredEmail === "test@user.com" && enteredPassword === "password";
+  };
+
   return (
     <div className="register w-50 p-3 d-flex flex-column justify-content-center">
-      <h4 style={{ color: `var(--primary-purple)`, textAlign: "center" }}>Masuk</h4>
-      <div className="mb-3">
+      <h4 style={{ color: `var(--primary-purple)`, textAlign: "center" }}>
+        Masuk
+      </h4>
+      <div className="mb-3 position-relative">
         <label htmlFor="formGroupExampleInput2" className="form-label">
           Email
         </label>
@@ -13,40 +78,89 @@ function Login() {
           type="email"
           className="form-control rounded-4"
           id="formGroupExampleInput2"
-          placeholder="jhonDoe@gmail.com"
+          placeholder="Contoh: jhonDoe@gmail.com"
+          value={email}
+          onChange={handleEmailChange}
         />
+        {email.length > 0 && (
+          <span className="position-absolute top-50 end-0 translate-middle-y">
+            {validEmail ? (
+              <FaCheckCircle
+                className="text-success"
+                style={{ marginRight: "10px", marginTop: "30px" }}
+              />
+            ) : (
+              <FaTimesCircle
+                className="text-danger"
+                style={{ marginRight: "10px", marginTop: "30px" }}
+              />
+            )}
+          </span>
+        )}
       </div>
-      <div className="mb-3">
+      <div className="mb-3 position-relative">
         <label htmlFor="formGroupExampleInput4" className="form-label">
           Password
         </label>
         <input
-          type="password"
+          type={showPassword ? "text" : "password"}
           className="form-control rounded-4"
           id="formGroupExampleInput4"
-          placeholder="........"
+          placeholder="Masukkan Password"
+          value={password}
+          onChange={handlePasswordChange}
         />
+        <span
+          className="position-absolute top-50 end-0 translate-middle-y"
+          onClick={togglePasswordVisibility}
+        >
+          {showPassword ? (
+            <FaEyeSlash
+              style={{ marginRight: "15px", marginTop: "35px", color: "grey" }}
+            />
+          ) : (
+            <FaEye
+              style={{ marginRight: "15px", marginTop: "35px", color: "grey" }}
+            />
+          )}
+        </span>
       </div>
       <div className="mb-3">
-        
-        <Link
-          to={`/myClass`}
+        <button
+          // to={`/myClass`}
           className="btn rounded-4 text-light"
           style={{ backgroundColor: `var(--primary-purple)`, width: "100%" }}
+          onClick={handleLogin}
         >
           Masuk
-        </Link>
+        </button>
       </div>
       <hr />
       <div className="text-center">
         <p>
           Belum punya akun?{" "}
           <span>
-            <Link to={`/register`} style={{ textDecoration: "none", color: `var(--primary-purple)`, fontWeight: "bold" }}>
+            <Link
+              to={`/register`}
+              style={{
+                textDecoration: "none",
+                color: `var(--primary-purple)`,
+                fontWeight: "bold",
+              }}
+            >
               Daftar di sini
             </Link>
           </span>
         </p>
+        {password.length > 0 && !passwordValid && (
+          <div className="btn button-danger">Password min 8 karakter!</div>
+        )}
+        {!isRegisteredEmail && (
+          <div className="btn button-danger">Alamat email tidak terdaftar!</div>
+        )}
+        {incorrectPassword && (
+          <div className="btn button-danger">Maaf, kata sandi salah!</div>
+        )}
       </div>
 
       {/* Media Query for Large Screens */}
@@ -58,6 +172,25 @@ function Login() {
               height: 100% !important;
               background-color: var(--primary-young-blue) !important;
             }
+          }
+
+          input {
+            height: 48px;
+          }
+
+          .button-danger {
+            background-color: #FF0000;
+            color: #fff;
+            border-radius: 15px;
+            padding: 15px;
+            font-weight: 500;
+            width: 60%;
+            margin-top: 20px;
+          }
+
+          .button-danger:hover {
+            background-color: #FF0000;
+            color: #fff;
           }
         `}
       </style>
