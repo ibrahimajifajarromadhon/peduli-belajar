5e1c434fa86c543bed4c2fdb510b358aae678c43
Ayuni Tiyasari <ayutiyassari@gmail.com>
2023-12-14 23:14:50 +0700

    update

--- src/components/user/ListAllCourse.jsx
+++ src/components/user/ListAllCourse.jsx
@@ -17,14 +17,14 @@ const ListAllCourse = ({filter}) => {
     const token = Cookies.get('token');
     try {
       const response = await axios.get(
-        `${import.meta.env.VITE_API}/api/course`,
+        `${import.meta.env.VITE_API}/api/course/filter?page=1&size=20&type=PREMIUM &type=GRATIS`,
         {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         }
       );
-      setListCourse(response.data.data);
+      setListCourse(response.data.data.courses);
     } catch (error) {
       console.log("terjadi kesalahan");
     }
@@ -38,9 +38,9 @@ const ListAllCourse = ({filter}) => {
     if (filter === "all") {
       return listCourse;
     } else if (filter === "premium") {
-      return listCourse.filter((course) => course.type === "PREMIUM");
+      return listCourse.filter((course) => course.price !== 0 );
     } else if (filter === "gratis") {
-      return listCourse.filter((course) => course.type === "GRATIS");
+      return listCourse.filter((course) => course.price === 0 );
     } else {
       return [];
     }
@@ -51,7 +51,7 @@ const ListAllCourse = ({filter}) => {
   return (
     <div className="row row-cols-1 row-cols-md-2 g-4 ">
       {filteredCourses.map((course) => (
-        <div className="col" key={course.courseCode}>
+        <div className="col" key={course.id}>
           <div className="card" style={{ borderRadius: "22px" }}>
             <img src={ImgCourse} className="card-img-top" alt="..."/>
             <div className="card-body">
@@ -60,7 +60,7 @@ const ListAllCourse = ({filter}) => {
                   {course.category}
                 </h5>
                 <div className="ms-auto">
-                  <FaStar style={{ color: "yellow" }} /> 4.8
+                  <FaStar style={{ color: "yellow" }} /> {course.rating}
                 </div>
               </div>
               <h6 className="card-title">{course.title}</h6>
@@ -85,7 +85,7 @@ const ListAllCourse = ({filter}) => {
                     fontWeight: "600",
                   }}
                 >
-                  10 Modul
+                  {course.modul} Modul
                 </p>
                 <RiTimeFill style={{ color: "#73CA5C", marginLeft: "30px" }} />{" "}
                 <p
@@ -99,7 +99,7 @@ const ListAllCourse = ({filter}) => {
                 </p>
               </div>
               <div>
-              {course.type === "GRATIS" ? (
+              {course.price === 0 ? (
                 <Link to={`/detailClass/${course.courseCode}`} className="btn btn-kelas">
                   Mulai Kelas
                 </Link>
@@ -122,160 +122,6 @@ const ListAllCourse = ({filter}) => {
           </div>
         </div>
       ))}
-      {/* <div className="col">
-        <div className="card" style={{borderRadius: "22px"}}>
-          <img src={ImgCourse} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <div className="d-flex">
-              <h5 className="card-title" style={{ color: "#6148FF" }}>
-                UI/UX Design
-              </h5>
-              <div className="ms-auto">
-                <FaStar style={{ color: "yellow" }} /> 4.8
-              </div>
-            </div>
-            <h6 className="card-title">Membuat Grid System dengan Figma</h6>
-            <p className="card-text">by Simon Doe</p>
-            <div className="d-flex">
-              <RiShieldStarLine style={{ color: "#73CA5C" }} />{" "}
-              <p
-                style={{
-                  textDecoration: "none",
-                  color: "#6148FF",
-                  fontSize: "12px",
-                  fontWeight: "600",
-                }}
-              >
-                Advanced Level
-              </p>
-              <RiBook3Line style={{ color: "#73CA5C", marginLeft: "30px" }} />{" "}
-              <p
-                style={{
-                  textDecoration: "none",
-                  fontSize: "12px",
-                  fontWeight: "600",
-                }}
-              >
-                10 Modul
-              </p>
-              <RiTimeFill style={{ color: "#73CA5C", marginLeft: "30px" }} />{" "}
-              <p
-                style={{
-                  textDecoration: "none",
-                  fontSize: "12px",
-                  fontWeight: "600",
-                }}
-              >
-                100 menit
-              </p>
-            </div>
-              <Link to={`/detailClass/WD2125`} className="btn btn-kelas">Mulai Kelas</Link>
-          </div>
-        </div>
-      </div>
-      <div className="col">
-        <div className="card" style={{borderRadius: "22px"}}>
-          <img src={ImgCourse} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <div className="d-flex">
-              <h5 className="card-title" style={{ color: "#6148FF" }}>
-                UI/UX Design
-              </h5>
-              <div className="ms-auto">
-                <FaStar style={{ color: "yellow" }} /> 4.8
-              </div>
-            </div>
-            <h6 className="card-title">Membuat Grid System dengan Figma</h6>
-            <p className="card-text">by Simon Doe</p>
-            <div className="d-flex">
-              <RiShieldStarLine style={{ color: "#73CA5C" }} />{" "}
-              <p
-                style={{
-                  textDecoration: "none",
-                  color: "#6148FF",
-                  fontSize: "12px",
-                  fontWeight: "600",
-                }}
-              >
-                Advanced Level
-              </p>
-              <RiBook3Line style={{ color: "#73CA5C", marginLeft: "30px" }} />{" "}
-              <p
-                style={{
-                  textDecoration: "none",
-                  fontSize: "12px",
-                  fontWeight: "600",
-                }}
-              >
-                10 Modul
-              </p>
-              <RiTimeFill style={{ color: "#73CA5C", marginLeft: "30px" }} />{" "}
-              <p
-                style={{
-                  textDecoration: "none",
-                  fontSize: "12px",
-                  fontWeight: "600",
-                }}
-              >
-                100 menit
-              </p>
-            </div>
-              <button className="btn btn-kelas" data-bs-toggle="modal" data-bs-target="#exampleModal" type='button'><IoDiamond />Premium</button>
-          <ModalBuyPremium />
-          </div>
-        </div>
-      </div>
-      <div className="col">
-        <div className="card" style={{borderRadius: "22px"}}>
-          <img src={ImgCourse} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <div className="d-flex">
-              <h5 className="card-title" style={{ color: "#6148FF" }}>
-                UI/UX Design
-              </h5>
-              <div className="ms-auto">
-                <FaStar style={{ color: "yellow" }} /> 4.8
-              </div>
-            </div>
-            <h6 className="card-title">Membuat Grid System dengan Figma</h6>
-            <p className="card-text">by Simon Doe</p>
-            <div className="d-flex">
-              <RiShieldStarLine style={{ color: "#73CA5C" }} />{" "}
-              <p
-                style={{
-                  textDecoration: "none",
-                  color: "#6148FF",
-                  fontSize: "12px",
-                  fontWeight: "600",
-                }}
-              >
-                Advanced Level
-              </p>
-              <RiBook3Line style={{ color: "#73CA5C", marginLeft: "30px" }} />{" "}
-              <p
-                style={{
-                  textDecoration: "none",
-                  fontSize: "12px",
-                  fontWeight: "600",
-                }}
-              >
-                10 Modul
-              </p>
-              <RiTimeFill style={{ color: "#73CA5C", marginLeft: "30px" }} />{" "}
-              <p
-                style={{
-                  textDecoration: "none",
-                  fontSize: "12px",
-                  fontWeight: "600",
-                }}
-              >
-                100 menit
-              </p>
-            </div>
-              <Link to={"/detailClass/1"} className="btn btn-kelas">Mulai Kelas</Link>
-          </div>
-        </div>
-      </div> */}
       <style>{`
       .btn-kelas  {
         padding: 5px;
