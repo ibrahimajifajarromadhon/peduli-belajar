54e3def5508879d1948d84bb10fee3e1af428a1b
ibrahimajifajarromadhon <ajii4262@gmail.com>
2023-12-15 20:54:29 +0700

    updated

--- src/components/user/PaymentHistory.jsx
+++ src/components/user/PaymentHistory.jsx
@@ -4,17 +4,17 @@ import imgCourse from "../../assets/image-course.png";
 import { IoDiamond } from "react-icons/io5";
 import { FaStar } from "react-icons/fa";
 import { RiShieldStarLine, RiBook3Line, RiTimeFill } from "react-icons/ri";
+import Cookies from "js-cookie";
 
-const PaymentHistory = ({ filter, email }) => { // Tambahkan props email
+const PaymentHistory = ({ filter }) => { // Tambahkan props email
   const [listPaymentHistory, setListPaymentHistory] = useState([]);
-
+console.log({listPaymentHistory: listPaymentHistory})
   useEffect(() => {
-    const getPaymentHistory = async (email) => {
-      console.log(email);
-      const token = localStorage.getItem("token");
+    const getPaymentHistory = async () => {
+      const token = Cookies.get("token");
       try {
         const response = await axios.get(
-          `${import.meta.env.VITE_API}/api/order/payment-history/${email}`,
+          `${import.meta.env.VITE_API}/api/order/payment-history`,
           {
             headers: {
               Authorization: `Bearer ${token}`,
@@ -28,38 +28,41 @@ const PaymentHistory = ({ filter, email }) => { // Tambahkan props email
     };
 
     getPaymentHistory();
-  }, [email]); 
-  const filterPaymentHistory = () => {
-    if (filter === "all") {
-      return listPaymentHistory;
-    } else if (filter === "paid") {
-      return listPaymentHistory.filter((payment) => payment.status === "Paid");
-    } else if (filter === "wait") {
-      return listPaymentHistory.filter((payment) => payment.status === "Waiting for Payment");
-    } else {
-      return [];
-    }
-  };
+  }, []); 
+  
+  // const filterPaymentHistory = () => {
+  //   if (filter === "all") {
+  //     return listPaymentHistory;
+  //   } else if (filter === "paid") {
+  //     return listPaymentHistory.filter((payment) => payment.status === "Paid");
+  //   } else if (filter === "wait") {
+  //     return listPaymentHistory.filter(
+  //       (payment) => payment.status === "Waiting for Payment"
+  //     );
+  //   } else {
+  //     return [];
+  //   }
+  // };
 
-  const filteredList = filterPaymentHistory(); 
+  // const filteredList = filterPaymentHistory(); 
 
   return (
     <div className="row row-cols-1 row-cols-md-1 g-4">
-      {filteredList.map((payment) => ( 
+      {listPaymentHistory.map((payment) => ( 
         <div className="col" key={payment.id}>
           <div className="card" style={{ borderRadius: "22px" }}>
-            <img src={imgCourse} className="card-img-top" alt={payment.courseName} />
+            <img src={imgCourse} className="card-img-top" alt="" />
             <div className="card-body">
             <div className="d-flex">
             <h5 className="card-title" style={{ color: "#6148FF" }}>
-              Web Development
+            {payment.category}
             </h5>
             <div className="ms-auto">
               <FaStar style={{ color: "yellow" }} /> 4.8
             </div>
           </div>
-          <h6 className="card-title">Membuat Grid System dengan Figma</h6>
-          <p className="card-text">by Simon Doe</p>
+          <h6 className="card-title">{payment.title}</h6>
+          <p className="card-text">by {payment.teacher}</p>
           <div className="d-flex">
             <RiShieldStarLine style={{ color: "#73CA5C" }} />{" "}
             <p
@@ -70,7 +73,7 @@ const PaymentHistory = ({ filter, email }) => { // Tambahkan props email
                 fontWeight: "600",
               }}
             >
-              Advanced Level
+              {payment.level} LEVEL
             </p>
             <RiBook3Line style={{ color: "#73CA5C", marginLeft: "30px" }} />{" "}
             <p
@@ -96,7 +99,7 @@ const PaymentHistory = ({ filter, email }) => { // Tambahkan props email
               <div className="btn-kelas">
                 <button 
                   className="btn rounded-4 text-light"
-                  style={{ backgroundColor: `var(--allert-red)`, fontWeight: "500" }}
+                  style={{backgroundColor: payment.status === "Paid" ? "#73CA5C" : payment.status === "Waiting for Payment" ? "var(--allert-red)" : "", fontWeight: "500" }}
                 >
                   <IoDiamond />
                   {payment.status}
