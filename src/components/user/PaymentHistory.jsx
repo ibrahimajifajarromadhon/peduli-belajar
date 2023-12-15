116bdcb195fdd9c23e2e9d3289e7dd77697928ea
yulian8989 <myulianto572002@gmail.com>
2023-12-15 20:56:52 +0700

    update payment history

--- src/components/user/PaymentHistory.jsx
+++ src/components/user/PaymentHistory.jsx
@@ -4,17 +4,16 @@ import imgCourse from "../../assets/image-course.png";
 import { IoDiamond } from "react-icons/io5";
 import { FaStar } from "react-icons/fa";
 import { RiShieldStarLine, RiBook3Line, RiTimeFill } from "react-icons/ri";
-
-const PaymentHistory = ({ filter, email }) => { // Tambahkan props email
+import Cookies from 'js-cookie';
+const PaymentHistory = ({ filter }) => { 
   const [listPaymentHistory, setListPaymentHistory] = useState([]);
-
+  console.log (listPaymentHistory)
   useEffect(() => {
-    const getPaymentHistory = async (email) => {
-      console.log(email);
-      const token = localStorage.getItem("token");
+    const getPaymentHistory = async () => {
+      const token = Cookies.get('token');
       try {
         const response = await axios.get(
-          `${import.meta.env.VITE_API}/api/order/payment-history/${email}`,
+          `${import.meta.env.VITE_API}/api/order/payment-history`,
           {
             headers: {
               Authorization: `Bearer ${token}`,
@@ -28,77 +27,48 @@ const PaymentHistory = ({ filter, email }) => { // Tambahkan props email
     };
 
     getPaymentHistory();
-  }, [email]); 
-  const filterPaymentHistory = () => {
-    if (filter === "all") {
-      return listPaymentHistory;
-    } else if (filter === "paid") {
-      return listPaymentHistory.filter((payment) => payment.status === "Paid");
-    } else if (filter === "wait") {
-      return listPaymentHistory.filter((payment) => payment.status === "Waiting for Payment");
-    } else {
-      return [];
-    }
-  };
-
-  const filteredList = filterPaymentHistory(); 
-
+  }, []); 
+  
   return (
     <div className="row row-cols-1 row-cols-md-1 g-4">
-      {filteredList.map((payment) => ( 
-        <div className="col" key={payment.id}>
-          <div className="card" style={{ borderRadius: "22px" }}>
-            <img src={imgCourse} className="card-img-top" alt={payment.courseName} />
+      {listPaymentHistory.map((payment) => (
+        <div className="col" key={payment.courseCode}>
+          <div className="card" style={{ borderRadius: "22px", boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)" }}>
+            <img src={imgCourse} className="card-img-top" alt={payment.title} />
             <div className="card-body">
-            <div className="d-flex">
-            <h5 className="card-title" style={{ color: "#6148FF" }}>
-              Web Development
-            </h5>
-            <div className="ms-auto">
-              <FaStar style={{ color: "yellow" }} /> 4.8
-            </div>
-          </div>
-          <h6 className="card-title">Membuat Grid System dengan Figma</h6>
-          <p className="card-text">by Simon Doe</p>
-          <div className="d-flex">
-            <RiShieldStarLine style={{ color: "#73CA5C" }} />{" "}
-            <p
-              style={{
-                textDecoration: "none",
-                color: "#6148FF",
-                fontSize: "12px",
-                fontWeight: "600",
-              }}
-            >
-              Advanced Level
-            </p>
-            <RiBook3Line style={{ color: "#73CA5C", marginLeft: "30px" }} />{" "}
-            <p
-              style={{
-                textDecoration: "none",
-                fontSize: "12px",
-                fontWeight: "600",
-              }}
-            >
-              10 Modul
-            </p>
-            <RiTimeFill style={{ color: "#73CA5C", marginLeft: "30px" }} />{" "}
-            <p
-              style={{
-                textDecoration: "none",
-                fontSize: "12px",
-                fontWeight: "600",
-              }}
-            >
-              100 menit
-            </p>
-          </div>
-              <div className="btn-kelas">
-                <button 
+              <div className="d-flex">
+                <h5 className="card-title" style={{ color: "#6148FF", fontWeight: "700" }}>
+                  {payment.category.replace(/_/g, " ")}
+                </h5>
+                <div className="ms-auto">
+                  <FaStar style={{ color: "yellow" }} /> {payment.rating} 
+                </div>
+              </div>
+              <h6 className="card-title" style={{ fontSize: "15px", fontWeight: "650" }}>{payment.title}</h6>
+              <p className="card-text mt-2 " style={{ fontSize: "14px", fontWeight: "500" }}>by {payment.teacher}</p>
+              <div className="d-flex mt-2">
+                <RiShieldStarLine style={{ color: "#73CA5C" }} />
+                <p style={{ textDecoration: "none", color: "#6148FF", fontSize: "12px", fontWeight: "600", marginLeft: "5px" }}>
+                  {payment.level} LEVEL
+                </p>
+                <RiBook3Line style={{ color: "#73CA5C", marginLeft: "30px" }} />
+                <p style={{ textDecoration: "none", fontSize: "12px", fontWeight: "600",marginLeft: "5px" }}>
+                  {payment.modul} Modul 
+                </p>
+                <RiTimeFill style={{ color: "#73CA5C", marginLeft: "30px" }} />
+                <p style={{ textDecoration: "none", fontSize: "12px", fontWeight: "600",marginLeft: "5px" }}>
+                  100 menit 
+                </p>
+              </div>
+              <div className="btn-kelas mt-3">
+                <button
                   className="btn rounded-4 text-light"
-                  style={{ backgroundColor: `var(--allert-red)`, fontWeight: "500" }}
+                  style={{
+                    backgroundColor: payment.status === "Waiting for Payment" ? "var(--allert-red)" : "var(--allert-green)",
+                    fontWeight: "500"
+                  }}
                 >
-                  <IoDiamond />
+                  <IoDiamond style= {{marginRight:"5px"}}/>
                   {payment.status}
                 </button>
               </div>
@@ -108,6 +78,7 @@ const PaymentHistory = ({ filter, email }) => { // Tambahkan props email
       ))}
     </div>
   );
+
 };
 
 export default PaymentHistory;
